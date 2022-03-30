51f5b6a624f6e76e1ecc9502ec2a807a27b97751
muccidavide <davidemucci0804@gmail.com>
2022-03-30 10:48:40 +0200

    Js 2 Complete

---
+++ jsnack2/main.js
@@ -0,0 +1,18 @@
+/* SNACK 2 
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+ */
+
+
+
+const userWord1 = prompt("INserisci una parola");
+const userWord2 = prompt("INserisci una parola");
+
+
+if (userWord1.length > userWord2.length){
+    console.log(userWord1 + " " +userWord2);
+} else if (userWord1.length < userWord2.length){
+    console.log(userWord2 + " " + userWord1 );
+} else {
+    console.log("le parole sono uguali");
+}
