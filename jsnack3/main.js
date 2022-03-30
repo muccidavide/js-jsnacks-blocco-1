21af1a5ad71d0589bd2d5922e0ba0c7787190948
muccidavide <davidemucci0804@gmail.com>
2022-03-30 11:57:30 +0200

    Jsnack3 : complete

---
+++ jsnack3/main.js
@@ -0,0 +1,19 @@
+/* SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+ */
+let arrayNumbers = [];
+
+for ( i = 0; i < 10; i++){
+    let NumbersUser = parseInt(prompt('Inserisci un numero'));
+     arrayNumbers.push(NumbersUser);
+}
+ console.log(arrayNumbers);
+
+let somma = 0;
+for (i = 0 ; i < arrayNumbers.length; i++){
+    somma += arrayNumbers[i]
+    
+}
+console.log(somma);
+
