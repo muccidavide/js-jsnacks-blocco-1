d2c791d2deb0ea283e0b69881e1f8aff9ab4dea4
muccidavide <davidemucci0804@gmail.com>
2022-03-30 16:26:20 +0200

    Jsnack 8

---
+++ jsnack8/main.js
@@ -0,0 +1,13 @@
+// Chiedi un numero di 4 cifre all'utente w calcola la somma di tutte le cifre che compongono il numero
+
+
+const userNumber = prompt("Inserisci un numero di 4 cifre");
+const firstNumber =parseInt(userNumber[0]) 
+const secondNumber = parseInt(userNumber[1]) 
+const thirdNumber = parseInt(userNumber[2]) 
+const fourthNumber = parseInt(userNumber[3]) 
+
+const somma = firstNumber + secondNumber + thirdNumber + fourthNumber
+
+console.log(firstNumber);
+console.log(somma);
