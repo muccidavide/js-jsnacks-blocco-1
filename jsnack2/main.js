6ba3195c2e12a1f023272392d1a08d6faca070dc
muccidavide <davidemucci0804@gmail.com>
2022-03-30 14:27:39 +0200

    Correction Js3

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -10,9 +10,12 @@ const userWord2 = prompt("INserisci una parola");
 
 
 if (userWord1.length < userWord2.length){
-    console.log(userWord1 + " " +userWord2);
+    console.log(userWord1)
+    console.log(userWord2);
 } else if (userWord1.length > userWord2.length){
-    console.log(userWord2 + " " + userWord1 );
+    console.log(userWord2)
+    console.log(userWord1);
+    
 } else {
     console.log("le parole sono uguali");
 }
--- jsnack3/main.js
+++ jsnack3/main.js
@@ -3,19 +3,12 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
  */
 
-let arrayNumbers = [];
 let somma = 0;
 
-for ( i = 0; i < 10; i++){
+for ( i = 0; i < 2; i++){
     let NumbersUser = parseInt(prompt('Inserisci un numero'));
-     arrayNumbers.push(NumbersUser);
+    somma += NumbersUser
 }
- console.log(arrayNumbers);
 
-
-for (i = 0 ; i < arrayNumbers.length; i++){
-    somma += arrayNumbers[i]
-    
-}
 console.log(somma);
 
